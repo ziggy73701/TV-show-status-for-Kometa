ee7eb394bd1fa47f979d159190176d5cb63453f3
Tim Leroy <timothe.leroy@gmail.com>
2025-04-20 14:54:38 +0100

    Logging fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo "$CRON cd /app && /usr/local/bin/python TSSK.py >> /var/log/cron.log 2>&1" > /etc/cron.d/tssk-cron
+echo "$CRON cd /app && /usr/local/bin/python TSSK.py 2>&1 | tee -a /var/log/cron.log" > /etc/cron.d/tssk-cron
 chmod 0644 /etc/cron.d/tssk-cron
 crontab /etc/cron.d/tssk-cron
-echo "[INFO] Starting cron with schedule: $CRON"
+echo "TSSK is starting with the following cron schedule: $CRON"
+touch /var/log/cron.log
 cron
 tail -f /var/log/cron.log
